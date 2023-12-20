339b1f047a6dee0fc6acf6444de684f840be66f4
erdaserhan <erhan209@gmail.com>
2023-12-20 14:48:57 +0100

    TI

--- templates/galerie.php
+++ templates/galerie.php
@@ -13,12 +13,12 @@
     <h1>Galerie</h1>
     <table>
         <tr>
-            <td><figure><a href="img/1280px_Caritasbrønden.jpg" data-lightbox="galerie"><img src="img/480px_Caritasbrønden.jpg" alt=""></a><figcaption>Caritasbrønden</figcaption></figure></td>
+            <td><figure><a href="img/1280px_København.jpg" data-lightbox="galerie"><img src="img/480px_København.jpg" alt=""></a><figcaption>København</figcaption></figure></td>            
             <td><figure><a href="img/1280px_Christiansborg.jpg" data-lightbox="galerie"><img src="img/480px_Christiansborg.jpg" alt=""></a><figcaption>Christiansborg</figcaption></figure></td></tr>
-            <tr><td><figure><a href="img/1280px_Caritasbrønden.jpg" data-lightbox="galerie"><img src="img/480px_frederiksborg-castle.jpg" alt=""></a><figcaption>Caritasbrønden</figcaption></figure></td>
-        
-        
-            <td><figure><a href="img/1280px_København.jpg" data-lightbox="galerie"><img src="img/480px_København.jpg" alt=""></a><figcaption>København</figcaption></figure></td></tr>
+
+            <tr><td><figure><a href="img/1280px_frederiksborg-castle.jpg" data-lightbox="galerie"><img src="img/480px_frederiksborg-castle.jpg" alt=""></a><figcaption>Caritasbrønden</figcaption></figure></td> 
+            <td><figure><a href="img/1280px_Caritasbrønden.jpg" data-lightbox="galerie"><img src="img/480px_Caritasbrønden.jpg" alt=""></a><figcaption>Caritasbrønden</figcaption></figure></td></tr>
+
             <tr><td><figure><a href="img/1280px_Magstaede_street.jpg" data-lightbox="galerie"><img src="img/480px_Magstaede_street.jpg" alt=""></a><figcaption>Magstaede Street</figcaption></figure></td>
             <td><figure><a href="img/1280px_sirene.jpg" data-lightbox="galerie"><img src="img/480px_sirene.jpg" alt=""></a><figcaption>Sirene</figcaption></figure></td>
         </tr>
